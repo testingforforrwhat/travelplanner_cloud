0c993695a481dedcdce0429eadcaed8a59b401bc
testingforforwhat <2273664Z@student.gla.ac.uk>
2025-05-26 15:57:22 +0800

    build(delete /src): migrate springboot to springcloud - gateway

--- gateway/src/main/java/com/test/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/test/gateway/GatewayApplication.java
@@ -2,8 +2,10 @@ package com.test.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient  // 能够发现和注册到 Nacos
 public class GatewayApplication {
 
     public static void main(String[] args) {
