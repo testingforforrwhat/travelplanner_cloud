5b6024f725b42a5958a2e2cd9753bb3e64cfe991
testingforforwhat <2273664Z@student.gla.ac.uk>
2025-05-26 12:32:11 +0800

    build:

---
+++ src/test/java/com/test/travelplanner_cloud/TravelplannerCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.travelplanner_cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelplannerCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
